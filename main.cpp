a5b84712eb06246d7cb1e73a8051a6b23d29573b
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2023-04-08 11:31:31 +0530

    Add State variable

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+#include <LiquidCrystal.h>;
+const int rs = 12, en = 11, d5 = 6, d6 = 5, d7 = 4, d8 = 3;
+LiquidCrystal lcd(rs, en, d5, d6, d7, d8);
+
+bool isAlarm = false, isStopwatch = false, isTimer = false;
+const int buzzer = 2, led = 13;
+const int pushbtn1 = 10;
+const int pushbtn2 = 9;
+const int pushbtn3 = 8;
+const int pushbtn4 = 7;
+
+int hour = 10, minute = 15, second = 5, pis = 0, mls = 0, lastTime = 0, now;
+int hourAlarm = 0, minuteAlarm = 0, secondAlarm = 0;
+int mode = 0;
+int flag = 0, flagAlarm = 0; // indicates 0 = AM, 1 = PM
+
+int btnstate1; // indicates pushbtn's state
+int btnstate2;
+int btnstate3;
+int btnstate4;
 
 void setup()
 {
